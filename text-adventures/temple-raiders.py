b7dd2466a92857e6efe08487a8e3b4feebb0bcf4
ao-craggs <simon.cragg@ao.com>
2020-01-12 14:45:57 +0000

    Refactored choices and hints into functions.   - Progressed the latest scene.

--- text-adventures/temple-raiders.py
+++ text-adventures/temple-raiders.py
@@ -1,46 +1,65 @@
 import os
 from colorama import Fore, Back, Style
 
+NL_1 = "\n"
 NL_2 = "\n\n"
 game_over = False
 hint_count = 0
 
 
-def printText(text, color, end="\n\n"):
+def printText(text, color=Fore.WHITE, end=NL_2):
     print(color, end="")
     print(text, end=end)
     print(Style.RESET_ALL, end="")
+
+
+def inputChoice(prompt, options, includeHint=False):
+    printText(prompt)
+    letters = ["A", "B", "C"]
+    num_of_options = len(options)
+    for i in range(0, num_of_options):
+        letter = letters[i]
+        option = options[i]
+        print("  [" + letter + "] " + option)
+    if includeHint is True:
+        printText("  [H] USE A HINT", Fore.MAGENTA + Style.BRIGHT, end=NL_1)
+    print()
+    choice = input(" > ").upper()
+    print()
+    return choice
 
 
 def invalidInput():
     printText("Invalid Input", Fore.RED)
 
 
+def useHint(hintText):
+    global hint_count
+    hint_count = hint_count + 1
+    printText(hintText, Fore.MAGENTA + Style.BRIGHT)
+
+
 def gameOver():
     global game_over
-    print(Fore.RED, end="")
-    print("GAME OVER")
-    print(Style.RESET_ALL)
+    print(Fore.WHITE + Back.RED + "GAME OVER" + Style.RESET_ALL)
     game_over = True
 
 
 os.system('cls')
 
 print()
-print(Fore.GREEN + Back.LIGHTBLACK_EX + Style.DIM + "Temple Raiders")
+print(Fore.GREEN + Back.LIGHTBLACK_EX + Style.BRIGHT + "TEMPLE RAIDERS")
 print(Style.RESET_ALL)
 
 
 while game_over is not True:
-    printText("You're stood at the entrance of the ancient temple ruins. " +
+    printText("You're stood at the entrance of an ancient temple ruins. "
               "Two large mossy stone slabs block your path!", Fore.CYAN)
 
-    print("What should you do next?", end=NL_2)
-    print("  [A] Try to prise open the two stone slabs.")
-    print("  [B] Look around the area for useful items.")
-    print("  [C] GIVE UP AND GO HOME!", end=NL_2)
-    choice = input(" > ").upper()
-    print()
+    choice = inputChoice("What should you do next?", [
+        "Try to prise open the two stone slabs.",
+        "Look around the area for useful items.",
+        "GIVE UP AND GO HOME!"])
 
     if choice == "A":
         printText("You're a strong guy but these rocks ain't movin'!", Fore.YELLOW)
@@ -49,11 +68,10 @@ while game_over is not True:
         while game_over is not True:
             printText("You found a robust looking tree branch!", Fore.CYAN)
 
-            print("What do you want to do?")
-            print("  [A] Use the tree branch to prise open the stone entrance.")
-            print("  [B] Discard the item.", end=NL_2)
-            choice = input(" > ").upper()
-            print()
+            choice = inputChoice("What do you want to do?", [
+                "Use the tree branch to prise open the stone entrance.",
+                "Discard the item."
+            ])
 
             if choice == "A":
 
@@ -64,39 +82,36 @@ while game_over is not True:
                     printText("You're in a dark passage. " +
                               "You appear to have stepped on a skeleton!", Fore.CYAN)
 
-                    print("What should you do next?", end=NL_2)
-                    print("  [A] Ignore it and slowly advance down the passage.")
-                    print("  [B] Examine the skeleton.", end=NL_2)
-                    choice = input(" > ").upper()
-                    print()
+                    choice = inputChoice("What should you do next?", [
+                        "Ignore it and slowly advance down the passage.",
+                        "Examine the skeleton."
+                    ])
 
                     if choice == "A":
-                        printText("Argh .. " +
-                                  "You fall down a huge pit and impail " +
-                                  "yourself at the bottom on iron spikes! " +
+                        printText("Argh .. "
+                                  "You fall down a huge pit and impail "
+                                  "yourself at the bottom on iron spikes! "
                                   "Ouch.", Fore.YELLOW)
 
                         gameOver()
 
                     elif choice == "B":
-                        printText("Seems this guy wasn't as lucky as you. " +
-                                  "You find a torch in his utility belt. " +
+                        printText("Seems this guy wasn't as lucky as you. "
+                                  "You find a torch in his utility belt. "
                                   "It works!", Fore.YELLOW)
 
                         while game_over is not True:
 
-                            printText("You shine the torch down the passage. " +
-                                      "It's lucky you did! " +
-                                      "A huge pit lays before you, " +
+                            printText("You shine the torch down the passage. "
+                                      "It's lucky you did! "
+                                      "A huge pit lays before you, "
                                       "leading to certain death!", Fore.CYAN)
 
-                            print("What should you do next?", end=NL_2)
-                            print("  [A] Jump in the pit.")
-                            print("  [B] Attempt to jump over the pit.")
-                            print("  [C] Look for another option.", end=NL_2)
-
-                            choice = input(" > ").upper()
-                            print()
+                            choice = inputChoice("What should you do next?", [
+                                "Jump in the pit.",
+                                "Attempt to jump over the pit.",
+                                "Look for another option."
+                            ])
 
                             if choice == "A":
                                 printText(
@@ -104,8 +119,9 @@ while game_over is not True:
                                 gameOver()
 
                             elif choice == "B":
-                                printText("You take your best run and jump. " + "Your arms reach the other side but " +
-                                          "you badly wind yourself against the pit " +
+                                printText("You take your best run and jump. "
+                                          "Your arms reach the other side but "
+                                          "you badly wind yourself against the pit "
                                           "and fall back to your untimely death.", Fore.YELLOW)
                                 gameOver()
 
@@ -115,29 +131,30 @@ while game_over is not True:
 
                                 while game_over is not True:
 
-                                    printText("You think you could reach it with a jump " +
+                                    printText("You think you could reach it with a jump "
                                               "and swing to the other side.", Fore.CYAN)
 
-                                    print("What do you want to do?", end=NL_2)
-                                    print("  [A] Make a jump for the rope.")
-                                    print(
-                                        "  [B] Attempt to jump over the pit.")
-                                    print(Fore.MAGENTA + Style.BRIGHT, end="")
-                                    print("  [H] USE A HINT." +
-                                          Style.RESET_ALL, end=NL_2)
+                                    choice = inputChoice("What do you want to do?", [
+                                        "Make a bold jump for the rope.",
+                                        "Attempt to jump over the pit."
+                                    ], True)
 
-                                    choice = input(" > ").upper()
-                                    print()
+                                    if choice == "A":
+                                        printText(
+                                            "You swing across the deadly pit with style!", Fore.YELLOW)
+                                        # while game_over is not True:
+                                        printText("next scene here", Fore.CYAN)
+                                    elif choice == "B":
+                                        printText("You take your best run and jump. "
+                                                  "Your arms reach the other side but "
+                                                  "you badly wind yourself against the pit "
+                                                  "and fall back to your untimely death.", Fore.YELLOW)
+                                        gameOver()
 
-                                    if choice == "H":
-                                        hint_count = hint_count + 1
-                                        print(Fore.MAGENTA +
-                                              Style.BRIGHT, end="")
-                                        print(
-                                            "HINT: Last year you won Ninja Warrior.", end=" ")
-                                        print(
-                                            "Your speciality was the ropes.", end=NL_2)
-                                        print(Style.RESET_ALL)
+                                    elif choice == "H":
+                                        useHint("Last year you won Ninja Warrior. "
+                                                "Your speciality was the ropes."
+                                                )
                                     else:
                                         invalidInput()
                             else:
@@ -147,14 +164,14 @@ while game_over is not True:
                         invalidInput()
 
             elif choice == "B":
-                printText("You throw the branch into " +
+                printText("You throw the branch into "
                           "the forest and alert a hungry bear!", Fore.YELLOW)
                 gameOver()
             else:
                 invalidInput()
 
     elif choice == "C":
-        printText("Like all the other quitters before you, " +
+        printText("Like all the other quitters before you, "
                   "you pack up and go home.", Fore.YELLOW)
         gameOver()
 
