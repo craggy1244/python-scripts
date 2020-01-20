59c2fab839e093f601b4ce8e7525ae9c79fd5386
ao-craggs <simon.cragg@ao.com>
2020-01-20 19:27:06 +0000

    Used a boolean variable to determine if a swap has taken place.

--- sorting/bubble-sort.py
+++ sorting/bubble-sort.py
@@ -5,15 +5,15 @@ print("oOoOoOoOoOo", end='\n\n')
 numbers = [4, 9, 7, 1, 3, 6, 2, 10, 8, 5]
 num_len = len(numbers)
 
-swaps = 1
-while swaps > 0:
-    swaps = 0
+swapped = True
+while swapped is True:
+    swapped = False
     for i in range(0, num_len - 1):
         if numbers[i] > numbers[i+1]:
             temp = numbers[i]
             numbers[i] = numbers[i+1]
             numbers[i+1] = temp
-            swaps = swaps + 1
+            swapped = True
 
 # output
 for i in range(0, num_len):
